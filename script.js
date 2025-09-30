e88078e08c64a52c383ea587673622d6b374d950
Lexxie Rousseau <lrouss01@villanova.edu>
2025-09-30 19:49:13 -0400

    Implement RadioLand API integration for station search

--- script.js
+++ script.js
@@ -25,6 +25,25 @@ function searchStation() {
     hideError();
     hideResults();
 
+// Build the API URL
+const apiUrl = `${API_BASE_URL}?` +
+`lat=${PHILLY_LAT}&lon=${PHILLY_LON}&` +
+`search_freq=none&callsign=${callsign}&request_type=4&` +
+`pi_code=none&sig_strength=null&startMiles=none&` +
+`miles=null&format=none&rxHeight=10&measurementUnit=meters`;
+// Make the fetch request
+fetch(apiUrl)
+ .then(response => response.json())
+ .then(data => {
+ hideLoading();
+ showResults(data);
+ })
+ .catch(error => {
+ hideLoading();
+ showError('Error connecting to RadioLand API. Please try again.');
+ console.error('API Error:', error);
+ });
+
     // TODO: STUDENTS COMPLETE THIS SECTION
     // 1. Build the API URL using the constants above and the callsign variable
     // 2. Make a fetch request to the RadioLand API
